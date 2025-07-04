751e2d720829db1ea86405549577be10dcee2f55
Alitm25 <alitmgoorabsar5@gmail.com>
2025-07-05 02:50:49 +0330

    install tanstackQuery, axios and toast & implement menu api

---
+++ src/api/config/ApiClient.ts
@@ -0,0 +1,39 @@
+import axios from "axios";
+import {toast} from "react-toastify";
+
+const apiClient = axios.create({
+    baseURL: 'https://nest.navaxcollege.com/api/',
+    timeout: 120000
+});
+
+export default apiClient;
+
+
+apiClient.interceptors.response.use(function (response) {
+    return response.data;
+}, function (error) {
+    if (error.response) {
+        if (error.response.status === 404) {
+            toast.error('The resource does not exist!');
+        }
+        else if (error.response.status === 400) {
+            toast.error('The sending parameters are not correct!');
+        }
+        else if (error.response.status === 401) {
+            toast.error('Please sign in!');
+        }
+        else if (error.response.status === 403) {
+            toast.error('Authentication failed, You do not have access to continue!');
+        }
+        else {
+            toast.error('Something went wrong, Please try again later.');
+        }
+    } else if (error.request) {
+        toast.error('Server error, Please try again later.');
+    } else {
+        toast.error('An error occured, Please try again later.');
+    }
+
+    return Promise.reject(error);
+})
+
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,16 @@
 import "@/styles/globals.css";
 import "@/styles/icons.css";
+
 import 'swiper/css';
 import 'swiper/css/autoplay';
 import 'swiper/css/navigation';
+import 'react-toastify/dist/ReactToastify.css'
+
 import type { AppProps } from "next/app";
 import {Layout} from "@/components";
 import {Lato, Quicksand} from "next/font/google";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {ToastContainer} from "react-toastify";
 
 const quicksand = Quicksand({
     subsets: ['latin'],
@@ -18,18 +23,30 @@ const lato = Lato({
 })
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-      <>
-          <style jsx global>{`
+    const queryClient = new QueryClient({
+        defaultOptions: {
+            queries: {
+                refetchOnWindowFocus: false,
+                refetchIntervalInBackground: false,
+                retry: 0,
+            }
+        }
+    })
+    return (
+        <>
+            <style jsx global>{`
                 html {
                     font-family: ${quicksand.style.fontFamily}, sans-serif;
                     --font-lato: ${lato.style.fontFamily}, sans-serif;
                 }
-          `}</style>
-          <Layout>
-              <Component {...pageProps} />
-          </Layout>
-      </>
+            `}</style>
+            <QueryClientProvider client={queryClient}>
+                <Layout>
+                    <Component {...pageProps} />
+                    <ToastContainer autoClose={false} hideProgressBar={false} closeOnClick={true} draggable={false} theme={"light"} position={"top-right"}/>
+                </Layout>
+            </QueryClientProvider>
+          </>
 
-  )
+      )
 }
