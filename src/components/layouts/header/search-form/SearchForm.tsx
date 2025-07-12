ba1df8cc0362663a3edefa6fa27255cd9dd27b04
Alitm25 <alitmgoorabsar5@gmail.com>
2025-07-13 02:56:56 +0330

    implement the RHF (React Hook Form) fundamentals

--- src/components/layouts/header/search-form/SearchForm.tsx
+++ src/components/layouts/header/search-form/SearchForm.tsx
@@ -1,16 +1,26 @@
 import React from 'react';
 import {IconBox} from "@/components";
+import {useForm} from "react-hook-form";
 
 interface Props {
     inputClassName?: string;
 }
 
+interface FromInput {
+    search_text: string;
+}
+
 export function SearchForm({inputClassName} :Props) {
     // TODO must developed some interactions
+    const {register, handleSubmit} = useForm<FromInput>();
+
+    const onSubmit = (data :FromInput) => {
+        console.log(data);
+    }
 
     return (
-            <form name="search-form" action="#" method="post" className="flex items-center">
-                <input type="text" name="search_text" placeholder="Search for items"
+            <form name="search-form" onSubmit={handleSubmit(onSubmit)} action="#" method="post" className="flex items-center">
+                <input type="text" {...register('search_text')} placeholder="Search for items"
                        className={`text-xsmall text-gray-400 border-gray-300 w-full focus:outline-none ${inputClassName}`}/>
                 <button type="submit"><IconBox icon={'icon-search'} size={22}/></button>
             </form>
