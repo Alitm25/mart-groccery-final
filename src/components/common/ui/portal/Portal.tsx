f9e89a0fda8825c16a4e2d8559bf9d03453cc1e4
Alitm25 <alitmgoorabsar5@gmail.com>
2025-07-21 02:02:25 +0330

    fix the responsive issues in modal

--- src/components/common/ui/portal/Portal.tsx
+++ src/components/common/ui/portal/Portal.tsx
@@ -1,6 +1,7 @@
 import {createPortal} from "react-dom";
 import React, {useEffect} from "react";
 
+
 interface Props {
     children: React.ReactNode;
     onClose: () => void;
@@ -16,9 +17,10 @@ export function Portal({children, onClose}: Props) {
         }
     }, []);
 
+
     return createPortal(
-        <div className={'fixed top-0 left-0 right-0 bottom-0 bg-[#010101] bg-opacity-70 z-10'} onClick={onClose}>
-            <div className={'relative'} onClick={ (e) => e.stopPropagation()}>
+        <div className={'fixed top-0 left-0 right-0 bottom-0 bg-[#010101] bg-opacity-70 z-10 flex justify-center items-center'} onClick={onClose}>
+            <div onClick={ (e) => e.stopPropagation()}>
                 {children}
             </div>
 
