720e4806c1e91bf2290363f446d209f20aac7718
Alitm25 <alitmgoorabsar5@gmail.com>
2025-07-26 16:49:35 +0330

    fix some style issues on SimpleProductCard.tsx

--- src/components/common/product/product-card/ProductCardButton.tsx
+++ src/components/common/product/product-card/ProductCardButton.tsx
@@ -9,22 +9,26 @@ interface Props {
 
 
 export function ProductCardButton({productData}: Props) {
-    const {addItem} = useBasket();
+    const {addItem, getItem} = useBasket();
+
+    const currentProductInBasket = getItem(productData.id);
 
     return (
         <div className="add-product">
-            <button onClick={() => addItem(productData)} className="flex items-center justify-center text-heading-sm text-green-200 hover:bg-[#FDC04033] hover:text-[#FDC040] transition-[background-color] border-[1px] rounded-[4px] bg-green-150 px-[10px] py-[5px]">Adds
-                +
-            </button>
-            <div
-                className="input-product__container hidden border-[1px] rounded-[4px] border-green-300 text-green-300 h-[30px] p-[3px]">
-                <input type="number" value="1"
-                       className="input-product h-[24px] w-[50px] border-0 focus:outline-none text-center"/>
-                <div className="flex flex-col justify-between">
-                    <IconBox icon={'up icon-angle-small-up'} size={10} />
-                    <IconBox icon={'down icon-angle-small-down'} size={10} />
-                </div>
-            </div>
+            {
+                currentProductInBasket ?
+                    <div className="input-product__container border-[1px] rounded-[4px] border-green-300 text-green-300 h-full p-[3px]">
+                        <div className="flex flex-col justify-between items-center">
+                            <IconBox icon={'up icon-angle-small-up'} size={10} />
+                            <IconBox icon={'down icon-angle-small-down'} size={10} />
+                        </div>
+                    </div>
+                :
+                    <button onClick={() => addItem(productData)} className="flex items-center justify-center text-heading-sm text-green-200 hover:bg-[#FDC04033] hover:text-[#FDC040] transition-[background-color] border-[1px] rounded-[4px] bg-green-150 px-[10px] py-[5px]">Adds +
+                    </button>
+            }
+
+
         </div>
     );
 };
--- src/components/common/product/product-card/SimpleProductCard.tsx
+++ src/components/common/product/product-card/SimpleProductCard.tsx
@@ -3,12 +3,16 @@ import Link from "next/link";
 import {EntityType} from "@/types";
 import {ProductsType} from "@/types/api/Products";
 import {ProductCardButton} from "@/components/common/product/product-card/ProductCardButton";
+import {useBasket} from "@/stores/basketContext";
 
 interface Props {
     data: EntityType<ProductsType>
 }
 
 export function SimpleProductCard({data}: Props) {
+    const {getItem} = useBasket();
+    const currentProductInBasket = getItem(data.id);
+
     return (
         <div className={'w-full'}>
             <div className="group border-[1px] border-gray-200 hover:border-green-150 transition-[border] rounded-[10px] hover:shadow-[20px_20px_40px_0_rgba(24,24,24,0.07)] relative p-3 md:p-4 xl:px-5 xl:pb-5 lg:pt-[65px] h-full">
@@ -60,7 +64,7 @@ export function SimpleProductCard({data}: Props) {
                                 </button>
                             </div>
                         </> :
-                        <div className="flex items-center justify-between mt-3">
+                        <div className={`flex items-center justify-between ${currentProductInBasket ? 'mt-0' : 'mt-3'}`}>
                             {
                                 data?.attributes?.sell_price ?
                                     <div>
