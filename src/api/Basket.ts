8b3fd7fda2f109167b7d32c8e3ce3dc6e2fe87e3
Alitm25 <alitmgoorabsar5@gmail.com>
2025-08-02 15:35:23 +0330

    implement online basket for non logged in users (without uuid and login token)

--- src/api/Basket.ts
+++ src/api/Basket.ts
@@ -4,6 +4,16 @@ import {BasketItemsType, updateBasket} from "@/types/api/Basket";
 
 
 export async function basketApiCall() :Promise<ApiResponseSingleType<BasketItemsType>> {
+    const token = window.localStorage.getItem('loginToken');
+    const uuid = window.localStorage.getItem('uuid');
+
+    if (!token && !uuid) {
+        const response :ApiResponseSingleType<BasketItemsType> = await apiClient.post('my-basket');
+        window.localStorage.setItem('uuid', response.data.attributes.uuid);
+
+        return response;
+    }
+
     return await apiClient.get('my-basket');
 }
 
