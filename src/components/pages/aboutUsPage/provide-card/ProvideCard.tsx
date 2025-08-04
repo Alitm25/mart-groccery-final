9e2b4407bd3e28fb6afc44b62189163a8e5b60de
Alitm25 <alitmgoorabsar5@gmail.com>
2025-08-05 03:18:52 +0330

    added ProvideCard.tsx data to about us page

--- src/components/pages/aboutUsPage/provide-card/ProvideCard.tsx
+++ src/components/pages/aboutUsPage/provide-card/ProvideCard.tsx
@@ -9,10 +9,10 @@ interface Props {
 
 export function ProvideCard({icon, title, description}: Props) {
     return (
-        <div className={'border border-[#E5E5E5] hover:border-green-300 rounded-xl py-46 px-32 flex flex-col items-center justify-between bg-white transition-all'}>
+        <div className={'border border-[#E5E5E5] hover:border-green-300 rounded-xl py-12 px-8 flex flex-col items-center justify-center gap-y-7 bg-white transition-all'}>
             <ImageView alt={'provider-icon'} width={100} height={100} src={icon}/>
             <h2 className={'font-quicksand text-heading4 font-bold'}>{title}</h2>
-            <p className={'font-lato text-lg font-normal text-[#7E7E7E]'}></p>
+            <p className={'font-lato text-lg font-normal text-[#7E7E7E] text-center'}>{description}</p>
             <Link href={'#'}>
                 <span className={'font-lato font-normal text-lg text-[#3BB77E]'}>Read more</span>
             </Link>
--- src/pages/about-us/index.tsx
+++ src/pages/about-us/index.tsx
@@ -3,6 +3,7 @@ import {FreeMode, Navigation, Thumbs} from "swiper/modules";
 import {IconBox, ImageView} from "@/components";
 import {useState} from "react";
 import {Section} from "@/components/section/Section";
+import {ProvideCard} from "@/components/pages/aboutUsPage";
 
 interface Props {
 
@@ -27,8 +28,26 @@ export default function Index({}: Props) {
                 </div>
             </Section>
             <Section>
-                <div className={'grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 '}>
-
+                <div className={'flex flex-col items-center mb-16'}>
+                    <h2 className={'text-heading1 font-quicksand text-[#253D4E] '}>What We Provide?</h2>
+                    <svg width="150" height="15" viewBox="0 0 150 15" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M0 7.5
+           C12.5 0, 37.5 15, 50 7.5
+           C62.5 0, 87.5 15, 100 7.5
+           C112.5 0, 137.5 15, 150 7.5"
+                              fill="none"
+                              stroke="#A7F3D0"
+                              stroke-width="6"
+                              stroke-linecap="round" />
+                    </svg>
+                </div>
+                <div className={'grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'}>
+                    <ProvideCard icon={'/assets/images/about-us/provide-card-images/005-sale.png'}      title={'Best Prices & Offers'}   description={'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form'} />
+                    <ProvideCard icon={'/assets/images/about-us/provide-card-images/Group.png'}         title={'Wide Assortment'}        description={'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form'} />
+                    <ProvideCard icon={'/assets/images/about-us/provide-card-images/Group(1).png'}      title={'Free Delivery'}          description={'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form'} />
+                    <ProvideCard icon={'/assets/images/about-us/provide-card-images/003-exchange.png'}  title={'Easy Returns'}           description={'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form'} />
+                    <ProvideCard icon={'/assets/images/about-us/provide-card-images/Group(2).png'}      title={'100% Satisfaction'}      description={'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form'} />
+                    <ProvideCard icon={'/assets/images/about-us/provide-card-images/surface1.png'}      title={'Great Daily Deal'}       description={'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form'} />
                 </div>
             </Section>
         </>
