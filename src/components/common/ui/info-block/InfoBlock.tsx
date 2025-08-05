55f0160ebdd7a0f1a3e692811ca04d72d0ad3b37
Alitm25 <alitmgoorabsar5@gmail.com>
2025-08-06 03:07:52 +0330

    implement InfoBlock.tsx

--- src/components/common/ui/info-block/InfoBlock.tsx
+++ src/components/common/ui/info-block/InfoBlock.tsx
@@ -1,11 +1,13 @@
 interface Props {
-
+    title: string;
+    description: string;
 };
 
-export function InfoBlock({}: Props) {
+export function InfoBlock({title, description}: Props) {
     return (
-        <div>
-
+        <div className={'flex items-start justify-start'}>
+            <h3 className={'font-quicksand text-heading3 mb-7 '}>{title}</h3>
+            <p className={'font-lato text-heading-sm text-[#7E7E7E]'}>{description}</p>
         </div>
     );
 };
--- src/pages/about-us/index.tsx
+++ src/pages/about-us/index.tsx
@@ -67,7 +67,9 @@ export default function Index({}: Props) {
                 </div>
             </Section>
             <Section>
+                <div className={'grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'}>
 
+                </div>
             </Section>
         </>
 
