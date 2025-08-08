ae474ace0c87c9fa355f0a3a6d75c56389ae4c3f
Alitm25 <alitmgoorabsar5@gmail.com>
2025-08-08 15:57:18 +0330

    fixed some responsive issues on TeamMemberCard.tsx

--- src/components/pages/aboutUsPage/team-member-card/TeamMemberCard.tsx
+++ src/components/pages/aboutUsPage/team-member-card/TeamMemberCard.tsx
@@ -15,11 +15,11 @@ interface Props {
 
 export function TeamMemberCard({img, name, job, link}: Props) {
     return (
-        <div className={'relative h-[571px] group'}>
+        <div className={'relative h-[500px] sm:h-[571px] group'}>
             <ImageView alt={'Team member image'} width={480} height={503} src={img} className={'rounded-lg group-hover:drop-shadow-2xl transition-all'}/>
-            <div className={'absolute z-[20] top-[45%] sm:top-[73%] left-[50%] translate-x-[-50%] w-3/4 flex flex-col items-center justify-center text-center bg-white rounded-2xl py-7 drop-shadow group-hover:drop-shadow-2xl transition-all'}>
-                <h3 className={'text-heading4 font-quicksand mb-2'}>{name}</h3>
-                <p className={'font-lato text-sm font-normal text-[#7E7E7E] mb-7'}>{job}</p>
+            <div className={'absolute z-[20] top-[50%] sm:top-[73%] left-[50%] translate-x-[-50%] w-3/4 flex flex-col items-center justify-center text-center bg-white rounded-2xl py-7 drop-shadow group-hover:drop-shadow-2xl transition-all'}>
+                <h3 className={'text-heading5 md:text-heading4 font-quicksand mb-2'}>{name}</h3>
+                <p className={'font-lato text-xs md:text-sm font-normal text-[#7E7E7E] mb-7'}>{job}</p>
                 <ul className={'flex items-center justify-center gap-x-4'}>
                     <li>
                         <Link href={link.facebook}>
