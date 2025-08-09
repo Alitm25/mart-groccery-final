660707559e3af6e563dc2e241f09c03e7d9a07a9
Alitm25 <alitmgoorabsar5@gmail.com>
2025-08-10 01:31:06 +0330

    implement single product page and added data based on api

--- src/pages/single-product/[id]/index.tsx
+++ src/pages/single-product/[id]/index.tsx
@@ -1,8 +1,10 @@
 import {Section} from "@/components/section/Section";
-import {Badge, IconBox, ImageView} from "@/components";
+import {Badge, IconBox, ImageView, Rating} from "@/components";
 import {useQuery} from "@tanstack/react-query";
 import {getAllProductsApiCall} from "@/api/Products";
 import {useRouter} from "next/router";
+import ProductCardButton from "@/components/common/product/product-card/ProductCardButton";
+import {useBasketData} from "@/hooks/useBasketData";
 
 interface Props {
 
@@ -11,15 +13,17 @@ interface Props {
 export default function Index({}: Props) {
     const router = useRouter();
     const {data: singleData} = useQuery({queryKey: ['single-product'], queryFn: () => getAllProductsApiCall({ filters: {id: router.query.id}, populate: ['thumbnail'] })});
+    const {addItem, getItem, updateProduct, basketItems} = useBasketData();
+
     console.log(singleData);
 
     return (
         <Section>
             {
                 singleData &&
-                <div className="container flex flex-col items-center justify-between mb-[68px]">
-                    <div className="flex flex-col md:flex-row justify-center w-4/5">
-                        <div className="w-1/2 mr-[28px]">
+                <div className="container flex flex-col items-center mb-[68px]">
+                    <div className="flex flex-col md:flex-row items-start justify-center w-full gap-x-8">
+                        <div>
                             <div className="w-full mb-[28px] p-[40px] border-[1px] border-gray-200 rounded-2xl">
                                 <div className="top-0 left-0 flex justify-end">
                                     <i className="icon-search text-[24px] text-gray-200 mb-20"></i>
@@ -50,11 +54,32 @@ export default function Index({}: Props) {
                                 </div>
                             }
                         </div>
-                        <div className="flex items-start justify-center">
-                            {singleData.data[0].attributes.label &&
-
-
+                        <div className="flex flex-col items-start justify-center w-4/12 gap-y-6">
+                            <div className={'py-1 px-3 rounded-md bg-[#DEF9EC] font-quicksand text-sm text-[#3BB77E] font-bold'}>
+                                In Stock
+                            </div>
+                            <h1 className={'text-heading2 font-quicksand text-[#253D4E]'}>{singleData.data[0].attributes.title}</h1>
+                            <div className={'flex items-center justify-center gap-x-[10px]'}>
+                                <Rating rate={singleData.data[0].attributes.rate} /> <span className={'text-xs font-lato font-normal text-[#7E7E7E]'}>({singleData.data[0].attributes.rate})</span>
+                            </div>
+                            {
+                                singleData.data[0].attributes.sell_price ?
+                                    <div className={'flex flex-row items-end justify-center gap-x-4'}>
+                                        <span className="font-quicksand font-bold text-7xl text-green-200">${singleData.data[0].attributes.sell_price}</span>
+                                        <span className="text-heading3 md:text-heading4 line-through text-gray-500">${singleData.data[0].attributes.price}</span>
+                                    </div>
+                                    : <span className="text-heading2 md:text-heading3 text-green-200">${singleData.data[0].attributes.price}</span>
                             }
+                            <p className={'font-lato font-normal text-lg text-[#7E7E7E]'}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam rem officia, corrupti reiciendis minima nisi modi, quasi, odio minus dolore impedit fuga eum eligendi? Officia doloremque facere quia. Voluptatum, accusantium!
+
+                                Uninhibited carnally hired played in whimpered dear gorilla koala depending and much yikes off far quetzal goodness and from for grimaced goodness.
+                            </p>
+                            <ProductCardButton productData={singleData.data[0]}/>
+
+                            <div className={'flex flex-row items-center justify-center gap-x-1'}>
+                                <span className={'font-lato text-lg font-normal text-[#253D4E]'}>SKU: </span>
+                                <span className={'font-lato text-lg font-normal text-[#B6B6B6]'}>{singleData.data[0].attributes.SKU}</span>
+                            </div>
                         </div>
                     </div>
                 </div>
