8d4dc123f54e10c153f2235bc58fcf955ac65053
Alitm25 <alitmgoorabsar5@gmail.com>
2025-08-11 02:42:22 +0330

    change some styling

--- src/api/Categories.ts
+++ src/api/Categories.ts
@@ -4,9 +4,10 @@ import {CategoryType} from "@/types/api/Category";
 
 interface Props {
     filters?: {}
+
 }
 
-export function getFeaturedCategories({filters = {}} :Props) :Promise<ApiResponseType<CategoryType>> {
+export function getAllCategories({filters = {}} :Props) :Promise<ApiResponseType<CategoryType>> {
     return apiClient.get('categories', {
         params: {
             populate: 'thumbnail',
