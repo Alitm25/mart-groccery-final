b6381929979dde26163aa4f4b4522b3d93ba6970
Alitm25 <alitmgoorabsar5@gmail.com>
2025-08-11 02:07:34 +0330

    fixed description section on render activation problem

--- src/pages/single-product/[id]/index.tsx
+++ src/pages/single-product/[id]/index.tsx
@@ -1,40 +1,24 @@
 import {Section} from "@/components/section/Section";
-import {Badge, IconBox, ImageView, InfoBlock, InfoBody, InfoBodyBlock, Rating} from "@/components";
+import {IconBox, ImageView, InfoBody, InfoBodyBlock, Rating} from "@/components";
 import {useQuery} from "@tanstack/react-query";
-import {getAllProductsApiCall} from "@/api/Products";
+import {getAllProductsApiCall, getSingleProduct} from "@/api/Products";
 import {useRouter} from "next/router";
 import ProductCardButton from "@/components/common/product/product-card/ProductCardButton";
 import {useBasketData} from "@/hooks/useBasketData";
-import {useEffect, useState} from "react";
-import {filter} from "minimatch";
+import {useState} from "react";
 
 
 export default function Index() {
     const router = useRouter();
-    const [showInfo, setShowInfo] = useState('');
-    const [isActive, setIsActive] = useState(true);
-    const {data: singleData} = useQuery({queryKey: ['single-product'], queryFn: () => getAllProductsApiCall({ filters: {id: router.query.id}, populate: ['thumbnail', 'categories'] })});
+    const [showInfo, setShowInfo] = useState('description');
+    const {data: singleData} = useQuery({queryKey: ['single-product'], queryFn: () => getSingleProduct(Number(router.query.id))});
     const {addItem, getItem, updateProduct, basketItems} = useBasketData();
-    const {data: relatedProducts} = useQuery({queryKey: ['category-based-products'], queryFn: () => getAllProductsApiCall(
-            {
-                filters: {
-                    categories: {
-                        title: {
-                            $eq: singleData?.data[0].attributes.categories?.data[0].attributes.title
-                        }
-                    }
-                }
-            }
-        )});
 
+    console.log(singleData);
 
     const showInfoHandler = (info :string) => {
         setShowInfo(info);
     }
-
-    useEffect(() => {
-        showInfoHandler('description');
-    }, []);
 
 
     return (
@@ -49,10 +33,10 @@ export default function Index() {
                                     <div className="top-0 left-0 flex justify-end">
                                         <i className="icon-search text-[24px] text-gray-200 mb-20"></i>
                                     </div>
-                                    <ImageView alt={'product image'} width={586} height={421} src={singleData.data[0].attributes.thumbnail?.data?.attributes.url}/>
+                                    <ImageView alt={'product image'} width={586} height={421} src={singleData.data.attributes.thumbnail?.data?.attributes.url}/>
                                 </div>
                                 {
-                                    singleData?.data[0].attributes.gallery &&
+                                    singleData?.data.attributes.gallery &&
                                     <div className="flex flex-row gap-[25px] justify-center items-center">
                                         <div className="bg-green-200 rounded-full flex flex-row-reverse items-center p-2">
                                             <IconBox icon={'icon-arrow-small-right rotate-180'} size={24}/>
@@ -79,27 +63,27 @@ export default function Index() {
                                 <div className={'py-1 px-3 rounded-md bg-[#DEF9EC] font-quicksand text-sm text-[#3BB77E] font-bold'}>
                                     In Stock
                                 </div>
-                                <h1 className={'text-heading3 md:text-heading2 font-quicksand text-[#253D4E]'}>{singleData.data[0].attributes.title}</h1>
+                                <h1 className={'text-heading3 md:text-heading2 font-quicksand text-[#253D4E]'}>{singleData.data.attributes.title}</h1>
                                 <div className={'flex items-center justify-center gap-x-[10px]'}>
-                                    <Rating rate={singleData.data[0].attributes.rate} /> <span className={'text-xs font-lato font-normal text-[#7E7E7E]'}>({singleData.data[0].attributes.rate})</span>
+                                    <Rating rate={singleData.data.attributes.rate} /> <span className={'text-xs font-lato font-normal text-[#7E7E7E]'}>({singleData.data.attributes.rate})</span>
                                 </div>
                                 {
-                                    singleData.data[0].attributes.sell_price ?
+                                    singleData.data.attributes.sell_price ?
                                         <div className={'flex flex-row items-end justify-center gap-x-4'}>
-                                            <span className="font-quicksand font-bold text-7xl text-green-200">${singleData.data[0].attributes.sell_price}</span>
-                                            <span className="text-heading3 md:text-heading4 line-through text-gray-500">${singleData.data[0].attributes.price}</span>
+                                            <span className="font-quicksand font-bold text-7xl text-green-200">${singleData.data.attributes.sell_price}</span>
+                                            <span className="text-heading3 md:text-heading4 line-through text-gray-500">${singleData.data.attributes.price}</span>
                                         </div>
-                                        : <span className="text-heading2 md:text-heading3 text-green-200">${singleData.data[0].attributes.price}</span>
+                                        : <span className="text-heading2 md:text-heading3 text-green-200">${singleData.data.attributes.price}</span>
                                 }
                                 <p className={'font-lato font-normal text-lg text-[#7E7E7E]'}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam rem officia, corrupti reiciendis minima nisi modi, quasi, odio minus dolore impedit fuga eum eligendi? Officia doloremque facere quia. Voluptatum, accusantium!
 
                                     Uninhibited carnally hired played in whimpered dear gorilla koala depending and much yikes off far quetzal goodness and from for grimaced goodness.
                                 </p>
-                                <ProductCardButton productData={singleData.data[0]}/>
+                                <ProductCardButton productData={singleData.data}/>
 
                                 <div className={'flex flex-row items-center justify-center gap-x-1'}>
                                     <span className={'font-lato text-lg font-normal text-[#253D4E]'}>SKU: </span>
-                                    <span className={'font-lato text-lg font-normal text-[#B6B6B6]'}>{singleData.data[0].attributes.SKU}</span>
+                                    <span className={'font-lato text-lg font-normal text-[#B6B6B6]'}>{singleData.data.attributes.SKU}</span>
                                 </div>
                             </div>
                         </div>
@@ -109,15 +93,15 @@ export default function Index() {
             <Section>
                 <div className={'flex flex-col items-start justify-between p-12 border border-[#F2F3F4] rounded-xl'}>
                     <div className={'flex flex-row flex-wrap items-center justify-center gap-x-4 md:gap-x-6 gap-y-6 mb-9'}>
-                        <button onClick={ () => showInfoHandler('description')} className={'px-5 py-2.5 md:px-[30px] md:py-[15px] rounded-full bg-white border border-[#F2F3F4] text-sm md:text-[18px] font-quicksand font-bold text-[#B6B6B6] hover:text-[#3BB77E] focus:text-[#3BB77E] hover:drop-shadow-xl focus:drop-shadow-xl transition-all'}> Description     </button>
-                        <button onClick={ () => showInfoHandler('additional')}  className={'px-5 py-2.5 md:px-[30px] md:py-[15px] rounded-full bg-white border border-[#F2F3F4] text-sm md:text-[18px] font-quicksand font-bold text-[#B6B6B6] hover:text-[#3BB77E] focus:text-[#3BB77E] hover:drop-shadow-xl focus:drop-shadow-xl transition-all'}> Additional info </button>
-                        <button onClick={ () => showInfoHandler('review')} className={'px-5 py-2.5 md:px-[30px] md:py-[15px] rounded-full bg-white border border-[#F2F3F4] text-sm md:text-[18px] font-quicksand font-bold text-[#B6B6B6] hover:text-[#3BB77E] focus:text-[#3BB77E] hover:drop-shadow-xl focus:drop-shadow-xl transition-all'}>Reviews         </button>
+                        <button onClick={ () => showInfoHandler('description')} className={`px-5 py-2.5 md:px-[30px] md:py-[15px] rounded-full bg-white border border-[#F2F3F4] text-sm md:text-[18px] font-quicksand font-bold hover:text-[#3BB77E] ${showInfo === 'description' ? 'text-[#3BB77E] drop-shadow-xl' : 'text-[#B6B6B6]'} hover:drop-shadow-xl  transition-all`}> Description     </button>
+                        <button onClick={ () => showInfoHandler('additional')}  className={`px-5 py-2.5 md:px-[30px] md:py-[15px] rounded-full bg-white border border-[#F2F3F4] text-sm md:text-[18px] font-quicksand font-bold  hover:text-[#3BB77E] ${showInfo === 'additional' ? 'text-[#3BB77E] drop-shadow-xl' : 'text-[#B6B6B6]'} hover:drop-shadow-xl transition-all`}> Additional info </button>
+                        <button onClick={ () => showInfoHandler('review')} className={`px-5 py-2.5 md:px-[30px] md:py-[15px] rounded-full bg-white border border-[#F2F3F4] text-sm md:text-[18px] font-quicksand font-bold  hover:text-[#3BB77E] ${showInfo === 'review' ? 'text-[#3BB77E] drop-shadow-xl' : 'text-[#B6B6B6]'} hover:drop-shadow-xl transition-all`}>Reviews         </button>
                     </div>
 
                     <div>
                         {
                             showInfo === 'description' ?
-                                <InfoBody description={singleData?.data[0].attributes.description} />
+                                <InfoBody description={singleData?.data.attributes.description} />
                             : showInfo === 'additional' ?
                                 <div className={'flex flex-col items-start gap-y-[15px]'}>
                                     <InfoBodyBlock title={'Packaging & Delivrey'} description={'Less lion goodness that euphemistically robin expeditiously bluebird smugly scratched far while thus cackled sheepishly rigid after due one assenting regarding censorious while occasional or this more crane went more as this less much amid overhung anathematic because much held one exuberantly sheep goodness so where rat wry well concomitantly.\n' +
