1bdd44e50a9b6e7eb663fb9ae740bba21433cece
Alitm25 <alitmgoorabsar5@gmail.com>
2025-08-15 03:22:59 +0330

    exported and added OrderContext.tsx custom hook

--- src/stores/OrderContext.tsx
+++ src/stores/OrderContext.tsx
@@ -1,4 +1,4 @@
-import {createContext, useState} from "react";
+import {createContext, useContext, useState} from "react";
 
 interface Props {
     children: React.ReactNode;
@@ -6,6 +6,7 @@ interface Props {
 
 
 const OrderContext = createContext({});
+export const useOrder = () => useContext(OrderContext);
 
 export function OrderContextProvider({children}: Props) {
     const [order, setOrder] = useState({});
