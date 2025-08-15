73721d5f09acd4fbb9d4f6895fc3e1b5cce22273
Alitm25 <alitmgoorabsar5@gmail.com>
2025-08-16 03:15:36 +0330

    fixed some styling in SingleProductButton.tsx

--- src/components/common/product/product-card/SingleProductButton.tsx
+++ src/components/common/product/product-card/SingleProductButton.tsx
@@ -18,7 +18,7 @@ function ProductCardButton({productData}: Props) {
         <div className="add-product">
             {
                 basketItem ?
-                    <div className={'border-[1px] font-quicksand font-bold rounded-[4px] border-green-300 text-green-300 h-full p-[3px] w-16 md:w-28 flex justify-evenly items-center'}>
+                    <div className={'border-[1px] font-quicksand font-bold text-xl rounded-[4px] border-green-300 text-green-300 h-full py-2 w-16 md:w-28 flex flex-row-reverse justify-evenly items-center'}>
                         <div className="flex flex-col justify-between items-center">
                             <IconBox icon={'up icon-angle-small-up cursor-pointer'} size={10} onClick={ () => updateProduct(productData.id, 'increase')}/>
                             <IconBox icon={'down icon-angle-small-down cursor-pointer'} size={10} onClick={ () => updateProduct(productData.id, 'decrease')}/>
@@ -26,7 +26,7 @@ function ProductCardButton({productData}: Props) {
                         {basketItem.quantity}
                     </div>
                     :
-                    <button onClick={() => addItem(productData.id)} className={"flex items-center justify-center text-heading-sm text-green-200 hover:bg-[#FDC04033] hover:text-[#FDC040] transition-[background-color] border-[1px] rounded-[4px] bg-green-150 px-[21px] py-[15px]"}>
+                    <button onClick={() => addItem(productData.id)} className={"flex items-center justify-center text-heading-sm text-white bg-[#3BB77E] hover:bg-white hover:text-[#3BB77E] hover:border-[#3BB77E] transition-[background-color] border-[1px] rounded-[4px] px-[21px] py-[15px]"}>
                         <IconBox icon={'icon-shopping-cart fill-white'} size={24} />
                         <span className={'hidden md:inline ml-1'}>Add To Cart</span>
                     </button>
