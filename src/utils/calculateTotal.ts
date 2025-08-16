8b3b1928b277cd769eb8eb8c64dae49fc7c8ac2f
Alitm25 <alitmgoorabsar5@gmail.com>
2025-08-17 02:29:45 +0330

    fixed type issues in calculateTotal.ts

--- src/utils/calculateTotal.ts
+++ src/utils/calculateTotal.ts
@@ -1,9 +1,9 @@
-import {BasketItemsType} from "@/types/api/Basket";
+import {basketItems} from "@/types/api/Basket";
 
-export default function calculateTotal(product :BasketItemsType) {
-    return product.basket_items.reduce( (acc, item) => {
-        const unitPrice = item.product.data.attributes.sell_price ?? item.product.data.attributes.price;
-        return acc + unitPrice * item.quantity
-    }, 0);
+export default function calculateTotal(product :basketItems[]) {
+        return product.reduce( (acc, item) => {
+                const unitPrice = item.product.data.attributes.sell_price ?? item.product.data.attributes.price;
+                return acc + unitPrice * item.quantity;
+        }, 0);
 
 }
