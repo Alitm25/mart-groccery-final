613c3a9388aad689ab43defd2f4a7c84f36ce4d5
Alitm25 <alitmgoorabsar5@gmail.com>
2025-08-18 01:43:27 +0330

    implement AccountSectionBtn.tsx

---
+++ src/components/pages/account/AccountSectionBtn.tsx
@@ -0,0 +1,15 @@
+import {ImageView} from "@/components";
+
+interface Props {
+    title: string;
+    icon: string;
+};
+
+export function AccountSectionBtn({title, icon}: Props) {
+    return (
+            <button className="flex cursor-pointer gap-3.5 text-black px-4 py-3 rounded-[10px] items-center border border-gray-100 hover:bg-green-200 hover:text-white">
+                <ImageView alt={'setting logo'} width={26} height={24} src={icon}/>
+                <div className="text-medium">title</div>
+            </button>
+    );
+};
