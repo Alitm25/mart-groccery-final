47ba0fa0d9d4c35125710c2d745aec2f8a2df8c3
Alitm25 <alitmgoorabsar5@gmail.com>
2025-08-18 02:13:31 +0330

    added select option in order to create the style of selecting in AccountSectionBtn.tsx

--- src/components/pages/account/AccountSectionBtn.tsx
+++ src/components/pages/account/AccountSectionBtn.tsx
@@ -4,11 +4,12 @@ interface Props {
     title: string;
     icon: string;
     onClick: () => void;
+    isSelect?: boolean;
 };
 
-export function AccountSectionBtn({title, icon, onClick}: Props) {
+export function AccountSectionBtn({title, icon, onClick, isSelect}: Props) {
     return (
-            <button onClick={onClick} className="flex cursor-pointer gap-3.5 text-black px-4 py-3 rounded-[10px] items-center border border-gray-100 hover:bg-green-200 hover:text-white">
+            <button onClick={onClick} className={`flex cursor-pointer gap-3.5 text-black px-4 py-3 rounded-[10px] items-center border border-gray-100 hover:bg-green-200 hover:text-white ${isSelect && 'bg-green-200 text-white'}`}>
                 <ImageView alt={'setting logo'} width={26} height={24} src={icon}/>
                 <div className="text-medium">{title}</div>
             </button>
