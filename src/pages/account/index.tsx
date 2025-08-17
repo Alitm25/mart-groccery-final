27798b3ce0fc65eaa551fce1b9fc74b4c42252c1
Alitm25 <alitmgoorabsar5@gmail.com>
2025-08-18 02:19:08 +0330

    implement account page fir account section components

--- src/pages/account/index.tsx
+++ src/pages/account/index.tsx
@@ -4,34 +4,51 @@ import {useModal} from "@/stores/ModalContext";
 import {useOrder} from "@/stores/OrderContext";
 import {YourOrders} from "@/components/pages/account/YourOrders";
 import {AccountSectionBtn} from "@/components/pages/account/AccountSectionBtn";
+import {useState} from "react";
 
-interface Props {
+type AccountSection = 'dashboard' | 'order-list' | 'track-orders' | 'my-address' | 'account-detail'
 
-};
-
-export default function Index({}: Props) {
-    const {openModal} = useModal();
-    const {order} =     useOrder();
+export default function Index() {
+    const {openModal}                         = useModal();
+    const {order}                             = useOrder();
+    const [accountSection, setAccountSection] = useState<AccountSection>('dashboard');
 
 
     return (
         <Section sectionClassName={"container lg:mt-[100px] sm:mt-4 font-lato mb-[239px]"}>
-            <div className="flex flex-wrap justify-center items-center">
-                {/*buttons*/}
-                <div className="flex flex-col gap-2.5 font-quickSand text-gray-500 text-heading6 lg:mr-[73px] sm:mr-0 sm:mb-10">
-                    <AccountSectionBtn title={'Dashbord'} icon={'/setting-logo.svg'} />
-                    <AccountSectionBtn title={'Order list'} icon={'/setting-logo.svg'} />
-                    <AccountSectionBtn title={'Track your orders'} icon={'/shop-logo.svg'} />
-                    <AccountSectionBtn title={'My address'} icon={'/location-logo.svg'} />
-                    <AccountSectionBtn title={'Account details'} icon={'/account-logo.svg'} />
-                    <AccountSectionBtn title={'Log out'} icon={'/order-logo.svg'} />
-
+            <div className={'flex items-center justify-evenly'}>
+                <div className="flex flex-wrap justify-center items-center">
+                    {/*buttons*/}
+                    <div className="flex flex-col gap-2.5 font-quickSand text-gray-500 text-heading6 ml-auto sm:mb-10">
+                        <AccountSectionBtn title={'Dashbord'} icon={'/setting-logo.svg'} onClick={ () => setAccountSection('dashboard')} isSelect={accountSection === 'dashboard'} />
+                        <AccountSectionBtn title={'Order list'} icon={'/setting-logo.svg'} onClick={ () => setAccountSection('order-list')} isSelect={accountSection === 'order-list'}/>
+                        <AccountSectionBtn title={'Track your orders'} icon={'/shop-logo.svg'} onClick={ () => setAccountSection('track-orders')} isSelect={accountSection === 'track-orders'}/>
+                        <AccountSectionBtn title={'My address'} icon={'/location-logo.svg'} onClick={ () => setAccountSection('my-address')} isSelect={accountSection === 'my-address'}/>
+                        <AccountSectionBtn title={'Account details'} icon={'/account-logo.svg'} onClick={ () => setAccountSection('account-detail')} isSelect={accountSection === 'account-detail'}/>
+                        <AccountSectionBtn title={'Log out'} icon={'/order-logo.svg'} onClick={() => openModal('ConfirmLogout')}/>
+                    </div>
                 </div>
+                <div>
+                    {/*sections*/}
+                    {
+                        accountSection === 'dashboard' ?
+                            <div>This is dashboard</div>
+                            : accountSection === 'order-list' ?
+                                <YourOrders order={order} />
+                                : accountSection === 'track-orders' ?
+                                    <div>This is track-orders</div>
+                                    : accountSection === 'my-address' ?
+                                        <div>This is my-address</div>
+                                        : accountSection === 'account-detail' ?
+                                            <div>This is account-details</div>
+                                            : null
 
-                {/*order details*/}
-
-                <YourOrders order={order} />
+                    }
+                </div>
             </div>
+
+
+
         </Section>
     );
 };
