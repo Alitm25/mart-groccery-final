68b1da705f6a3ceab194ed08e89aa8a53b3e303c
Alitm25 <alitmgoorabsar5@gmail.com>
2025-08-18 01:49:01 +0330

    added onClick function to AccountSectionBtn.tsx

--- src/components/pages/account/AccountSectionBtn.tsx
+++ src/components/pages/account/AccountSectionBtn.tsx
@@ -3,11 +3,12 @@ import {ImageView} from "@/components";
 interface Props {
     title: string;
     icon: string;
+    onClick: () => void;
 };
 
-export function AccountSectionBtn({title, icon}: Props) {
+export function AccountSectionBtn({title, icon, onClick}: Props) {
     return (
-            <button className="flex cursor-pointer gap-3.5 text-black px-4 py-3 rounded-[10px] items-center border border-gray-100 hover:bg-green-200 hover:text-white">
+            <button onClick={onClick} className="flex cursor-pointer gap-3.5 text-black px-4 py-3 rounded-[10px] items-center border border-gray-100 hover:bg-green-200 hover:text-white">
                 <ImageView alt={'setting logo'} width={26} height={24} src={icon}/>
                 <div className="text-medium">{title}</div>
             </button>
