54830c60b36d3e083c5190a42f6781bf86d5d01b
Alitm25 <alitmgoorabsar5@gmail.com>
2025-08-18 01:46:54 +0330

    added AccountSectionBtn.tsx to account page

--- src/pages/account/index.tsx
+++ src/pages/account/index.tsx
@@ -3,6 +3,7 @@ import {ImageView} from "@/components";
 import {useModal} from "@/stores/ModalContext";
 import {useOrder} from "@/stores/OrderContext";
 import {YourOrders} from "@/components/pages/account/YourOrders";
+import {AccountSectionBtn} from "@/components/pages/account/AccountSectionBtn";
 
 interface Props {
 
@@ -18,41 +19,13 @@ export default function Index({}: Props) {
             <div className="flex flex-wrap justify-center items-center">
                 {/*buttons*/}
                 <div className="flex flex-col gap-2.5 font-quickSand text-gray-500 text-heading6 lg:mr-[73px] sm:mr-0 sm:mb-10">
-                    <button className="flex cursor-pointer gap-3.5 text-black px-4 py-3 rounded-[10px] items-center border border-gray-100 hover:bg-green-200 hover:text-white">
-                        <ImageView alt={'setting logo'} width={26} height={24} src={'/setting-logo.svg'}/>
-                        <div className="text-medium">Dashboard</div>
-                    </button>
+                    <AccountSectionBtn title={'Dashbord'} icon={'/setting-logo.svg'} />
+                    <AccountSectionBtn title={'Order list'} icon={'/setting-logo.svg'} />
+                    <AccountSectionBtn title={'Track your orders'} icon={'/shop-logo.svg'} />
+                    <AccountSectionBtn title={'My address'} icon={'/location-logo.svg'} />
+                    <AccountSectionBtn title={'Account details'} icon={'/account-logo.svg'} />
+                    <AccountSectionBtn title={'Log out'} icon={'/order-logo.svg'} />
 
-                    <button
-                        className="flex cursor-pointer gap-3.5 text-black px-4 py-3 rounded-[10px] items-center border border-gray-100 hover:bg-green-200 hover:text-white">
-                        <ImageView alt={'setting logo'} width={26} height={24} src={'/setting-logo.svg'}/>
-                        <div className="text-medium">Order list</div>
-                    </button>
-
-                    <button
-                        className="flex cursor-pointer gap-3.5 text-black px-4 py-3 rounded-[10px] items-center border border-gray-100 hover:bg-green-200 hover:text-white">
-                        <ImageView alt={'shop logo'} width={26} height={24} src={'/shop-logo.svg'}/>
-
-                        <div className="text-medium">Track your orders</div>
-                    </button>
-
-                    <button
-                        className="flex cursor-pointer gap-3.5 text-black px-4 py-3 rounded-[10px] items-center border border-gray-100 hover:bg-green-200 hover:text-white">
-                        <ImageView alt={'location logo'} width={26} height={24} src={'/location-logo.svg'}/>
-                        <div className="text-medium">My address</div>
-                    </button>
-
-                    <button
-                        className="flex cursor-pointer gap-3.5 text-black px-4 py-3 rounded-[10px] items-center border border-gray-100 hover:bg-green-200 hover:text-white">
-                        <ImageView alt={'location logo'} width={26} height={24} src={'/account-logo.svg'}/>
-                        <div className="text-medium">Account details</div>
-                    </button>
-
-                    <button onClick={() => openModal('ConfirmLogout')}
-                        className="flex cursor-pointer gap-3.5 text-black px-4 py-3 rounded-[10px] items-center border border-gray-100 hover:bg-green-200 hover:text-white">
-                        <ImageView alt={'payment method'} width={26} height={24} src={'/order-logo.svg'}/>
-                        <div className="text-medium">Log out </div>
-                    </button>
                 </div>
 
                 {/*order details*/}
