1411ff8d29a43868f7e9421546444be84fd2e745
Alitm25 <alitmgoorabsar5@gmail.com>
2025-08-19 02:41:25 +0330

    fixed some styling

--- src/components/pages/account/YourOrders.tsx
+++ src/components/pages/account/YourOrders.tsx
@@ -11,30 +11,30 @@ export function YourOrders({ order }: Props) {
             <div className="overflow-x-auto">
                 <table className="min-w-[600px] w-full text-left border-collapse">
                     <thead>
-                    <tr className="bg-[#F5F5F5]">
-                        <th className="px-6 py-4 text-heading-sm lg:text-heading6 font-quicksand text-[#253D4E]">Order ID</th>
-                        <th className="px-6 py-4 text-heading-sm lg:text-heading6 font-quicksand text-[#253D4E]">Date</th>
-                        <th className="px-6 py-4 text-heading-sm lg:text-heading6font-quicksand text-[#253D4E]">Status</th>
-                        <th className="px-6 py-4 text-heading-sm lg:text-heading6 font-quicksand text-[#253D4E]">Total</th>
-                        <th className="px-6 py-4 text-heading-sm lg:text-heading6 font-quicksand text-[#253D4E]">Actions</th>
-                    </tr>
+                        <tr className="bg-[#F5F5F5]">
+                            <th className="px-6 py-4 text-heading-sm lg:text-heading6 font-quicksand text-[#253D4E]">Order ID</th>
+                            <th className="px-6 py-4 text-heading-sm lg:text-heading6 font-quicksand text-[#253D4E]">Date</th>
+                            <th className="px-6 py-4 text-heading-sm lg:text-heading6font-quicksand text-[#253D4E]">Status</th>
+                            <th className="px-6 py-4 text-heading-sm lg:text-heading6 font-quicksand text-[#253D4E]">Total</th>
+                            <th className="px-6 py-4 text-heading-sm lg:text-heading6 font-quicksand text-[#253D4E]">Actions</th>
+                        </tr>
                     </thead>
                     <tbody>
-                    {order.map((order) => (
-                        <tr key={order.id} className="border-b last:border-none hover:bg-gray-50">
-                            <td className="px-4 py-2 lg:px-6 lg:py-4 text-sm lg:text-medium font-lato text-[#7E7E7E]">#{order.id}</td>
-                            <td className="px-4 py-2 lg:px-6 lg:py-4 text-sm lg:text-medium font-lato text-[#7E7E7E]">{order.date}</td>
-                            <td className={`px-4 py-2 lg:px-6 lg:py-4 text-sm lg:text-medium font-lato ${order.status === 'processing' ? 'text-[#FDC040]' : 'text-[#81B13D]'}`}>
-                                {order.status}
-                            </td>
-                            <td className="px-4 py-2 lg:px-6 lg:py-4 text-sm lg:text-medium font-lato text-[#7E7E7E]">
-                                ${order.total} for {order.items.length} item
-                            </td>
-                            <td className="px-4 py-2 lg:px-6 lg:py-4">
-                                <button className="text-sm lg:text-medium font-lato text-[#3BB77E]">View</button>
-                            </td>
-                        </tr>
-                    ))}
+                        {order.map((order) => (
+                            <tr key={order.id} className="border-b last:border-none hover:bg-gray-50">
+                                <td className="px-4 py-2 lg:px-6 lg:py-4 text-sm lg:text-medium font-lato text-[#7E7E7E]">#{order.id}</td>
+                                <td className="px-4 py-2 lg:px-6 lg:py-4 text-sm lg:text-medium font-lato text-[#7E7E7E]">{order.date}</td>
+                                <td className={`px-4 py-2 lg:px-6 lg:py-4 text-sm lg:text-medium font-lato ${order.status === 'processing' ? 'text-[#FDC040]' : 'text-[#81B13D]'}`}>
+                                    {order.status}
+                                </td>
+                                <td className="px-4 py-2 lg:px-6 lg:py-4 text-sm lg:text-medium font-lato text-[#7E7E7E]">
+                                    ${order.total} for {order.items.length} item
+                                </td>
+                                <td className="px-4 py-2 lg:px-6 lg:py-4">
+                                    <button className="text-sm lg:text-medium font-lato text-[#3BB77E]">View</button>
+                                </td>
+                            </tr>
+                        ))}
                     </tbody>
                 </table>
             </div>
