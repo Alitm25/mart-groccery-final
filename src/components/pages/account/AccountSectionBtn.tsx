fd9d38365d3a7bae8af48ea2f7493b3a78e3f65f
Alitm25 <alitmgoorabsar5@gmail.com>
2025-08-19 02:35:50 +0330

    fixed some code styling

--- src/components/pages/account/AccountSectionBtn.tsx
+++ src/components/pages/account/AccountSectionBtn.tsx
@@ -9,7 +9,8 @@ interface Props {
 
 export function AccountSectionBtn({title, icon, onClick, isSelect}: Props) {
     return (
-            <button onClick={onClick} className={`flex flex-col lg:flex-row cursor-pointer gap-3.5 text-black px-4 py-3 rounded-[10px] items-center border border-gray-100 hover:bg-[#3BB77E] focus:text-white focus:drop-shadow-lg focus:scale-105 hover:text-white hover:drop-shadow-lg transition-all hover:scale-105 ${isSelect && 'bg-[#3BB77E] text-white drop-shadow-lg scale-105'}`}>
+            <button onClick={onClick}
+                className={`flex flex-col lg:flex-row cursor-pointer gap-3.5 text-black px-4 py-3 rounded-[10px] items-center border border-gray-100 hover:bg-[#3BB77E] focus:text-white focus:drop-shadow-lg focus:scale-105 hover:text-white hover:drop-shadow-lg transition-all hover:scale-105 ${isSelect && 'bg-[#3BB77E] text-white drop-shadow-lg scale-105'}`}>
                 <ImageView alt={'setting logo'} width={26} height={24} src={icon}/>
                 <div className="hidden lg:block text-medium">{title}</div>
             </button>
