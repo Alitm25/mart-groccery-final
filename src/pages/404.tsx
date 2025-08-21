0cbc5cd0a4664e640b9bd09a98bb0f7ee36e9b87
Alitm25 <alitmgoorabsar5@gmail.com>
2025-08-22 02:18:35 +0330

    fixed 404 page input ui issue

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -9,12 +9,22 @@ export default function Custom404() {
                 <ImageView alt={'404 logo'} width={510} height={194} src={'/assets/images/404.png'}/>
                 <h1 className={'text-heading1 font-quicksand text-[#253D4E] mb-7'}>Page Not Found</h1>
                 <p className={'text-heading-sm font-normal font-lato text-[#7E7E7E] mb-1'}>The link you clicked may be broken or the page may have been removed. </p>
-                   <p className={'text-heading-sm font-normal font-lato text-[#7E7E7E] mb-7'}>visit the <Link href={'/'}><span className={'text-[#3BB77E]'}>Homepage</span></Link>  or <Link href={'/contact-us'}><span className={'text-[#3BB77E]'}>Contact us</span></Link> about the problem </p>
-                <div className={'w-4/12 border border-[#E5E5E5] rounded-2xl drop-shadow hover:drop-shadow-lg flex items-center justify-center gap-x-2 transition-all p-4'}>
-                    <IconBox icon={'icon-search'} size={22}/>
-                    <div className={'w-full'}>
-                        <input type={'text'} placeholder={'Enter your keywords...'} className={'w-full text-base font-lato font-normal text-[#B6B6B6] outline-none'}/>
-                    </div>
+                <p className={'text-heading-sm font-normal font-lato text-[#7E7E7E] mb-7'}>visit the <Link href={'/'}><span className={'text-[#3BB77E]'}>Homepage</span></Link>  or <Link href={'/contact-us'}><span className={'text-[#3BB77E]'}>Contact us</span></Link> about the problem </p>
+                {/*<div className="w-full max-w-md border border-[#E5E5E5] rounded-2xl drop-shadow hover:drop-shadow-lg flex items-center gap-x-2 transition-all p-4 relative">*/}
+                {/*    <IconBox icon="icon-search" size={22} />*/}
+                {/*    <input*/}
+                {/*        type="text"*/}
+                {/*        placeholder="Enter your keywords..."*/}
+                {/*        className="flex-grow w-full h-full text-base font-lato font-normal text-[#B6B6B6] outline-none"*/}
+                {/*    />*/}
+                {/*</div>*/}
+                <div className="flex items-center w-full max-w-md rounded-xl border border-gray-200 shadow-sm px-[15px] py-5 bg-white hover:drop-shadow-xl transition">
+                    <IconBox icon="icon-search mr-1" size={22} />
+                    <input
+                        type="text"
+                        placeholder="Enter your keywords..."
+                        className="flex-1 bg-transparent font-lato outline-none ml-2 text-medium text-[#B6B6B6] placeholder-[#B6B6B6]"
+                    />
                 </div>
             </div>
         </div>
