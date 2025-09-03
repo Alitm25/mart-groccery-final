ec809ed2d606ef1878925a7f10ac852c581082b3
Alitm25 <alitmgoorabsar5@gmail.com>
2025-09-04 03:13:16 +0330

    implement ClearCartConfirmation.tsx

---
+++ src/components/pages/your-card/ClearCartConfirmation.tsx
@@ -0,0 +1,27 @@
+import {Modal} from "@/components";
+import {useModal} from "@/stores/ModalContext";
+import {useBasketData} from "@/hooks/useBasketData";
+
+
+export default function ClearCartConfirmation() {
+    const {closeModal} = useModal()
+    const {clearBasket} = useBasketData();
+
+
+    return (
+        <Modal closeModal={closeModal} title={'Confirm Logout'}>
+            <div className={'flex flex-col items-center justify-center gap-y-11'}>
+                <div className={'font-quicksand text-lg lg:text-2xl font-bold'}>Are you sure that you want clear all of the products?</div>
+
+                <div className={'flex items-center justify-evenly w-full'}>
+                    <button onClick={closeModal} className={'rounded-xl border-2 border-green-300 text-green-300 hover:bg-green-300 hover:text-white hover:border-white font-quicksand font-bold text-lg md:py-4 md:px-[38px] py-4 px-7 transition-all'}>
+                        No
+                    </button>
+                    <button onClick={() => {clearBasket(), closeModal()} } className={'rounded-xl border-2 border-green-300 text-green-300 hover:bg-green-300 hover:text-white hover:border-white font-quicksand font-bold text-lg md:py-4 md:px-[38px] py-4 px-7  transition-all'}>
+                        Yes
+                    </button>
+                </div>
+            </div>
+        </Modal>
+    );
+};
