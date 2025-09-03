fab521bb9b58531b5137c070305bdec228887b51
Alitm25 <alitmgoorabsar5@gmail.com>
2025-09-04 03:27:34 +0330

    implement the ClearCartConfirmation.tsx in order to fix some bugs about not rendering the latest basket data

--- src/components/pages/your-card/ClearCartConfirmation.tsx
+++ src/components/pages/your-card/ClearCartConfirmation.tsx
@@ -1,15 +1,31 @@
 import {Modal} from "@/components";
 import {useModal} from "@/stores/ModalContext";
 import {useBasketData} from "@/hooks/useBasketData";
+import {useRouter} from "next/router";
+import {toast} from "react-toastify";
+import {useQueryClient} from "@tanstack/react-query";
 
 
-export default function ClearCartConfirmation() {
+interface Props {
+    onClose: () => void;
+}
+
+export default function ClearCartConfirmation({onClose} :Props) {
+    const queryClient = useQueryClient();
     const {closeModal} = useModal()
     const {clearBasket} = useBasketData();
+    const router = useRouter();
 
+    const confirmHandler = () => {
+        clearBasket();
+        toast.success('Your basket has been cleared successfully');
+        closeModal();
+        router.push('/');
+        queryClient.invalidateQueries({queryKey: ['get-basket']})
+    }
 
     return (
-        <Modal closeModal={closeModal} title={'Confirm Logout'}>
+        <Modal closeModal={closeModal} title={'Confirm cleaning basket'}>
             <div className={'flex flex-col items-center justify-center gap-y-11'}>
                 <div className={'font-quicksand text-lg lg:text-2xl font-bold'}>Are you sure that you want clear all of the products?</div>
 
@@ -17,7 +33,7 @@ export default function ClearCartConfirmation() {
                     <button onClick={closeModal} className={'rounded-xl border-2 border-green-300 text-green-300 hover:bg-green-300 hover:text-white hover:border-white font-quicksand font-bold text-lg md:py-4 md:px-[38px] py-4 px-7 transition-all'}>
                         No
                     </button>
-                    <button onClick={() => {clearBasket(), closeModal()} } className={'rounded-xl border-2 border-green-300 text-green-300 hover:bg-green-300 hover:text-white hover:border-white font-quicksand font-bold text-lg md:py-4 md:px-[38px] py-4 px-7  transition-all'}>
+                    <button onClick={confirmHandler} className={'rounded-xl border-2 border-green-300 text-green-300 hover:bg-green-300 hover:text-white hover:border-white font-quicksand font-bold text-lg md:py-4 md:px-[38px] py-4 px-7  transition-all'}>
                         Yes
                     </button>
                 </div>
